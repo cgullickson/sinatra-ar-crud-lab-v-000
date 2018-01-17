e8afd134b320d746139963cdf3c1ae071d584cfd
Chris Gullickson <cgullickson5@gmail.com>
2018-01-17 23:12:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,12 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id' do
     @post = Post.find(params[:id])
-    erb :edit
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit 
   end
 
   patch '/posts/:id' do
